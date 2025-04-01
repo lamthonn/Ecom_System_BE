b3a8598b8c469d25e57fa01bd72905e4ff0d1ac1
lamthonn <lamvu180903@gmail.com>
2025-04-01 16:08:43 +0700

    update entity

--- Ecom/Entity/danh_gia.cs
+++ Ecom/Entity/danh_gia.cs
@@ -6,8 +6,9 @@ namespace Ecom.Entity
     public class danh_gia : BaseModel, IEntity
     {
         public Guid id { get; set; }
-        public Guid san_pham_id { get; set; }
+        public Guid? san_pham_id { get; set; }
         public Guid account_id { get; set; }
+        public string? ma_san_pham { get; set; }
         public int danh_gia_chat_luong { get; set; }
         public string? noi_dung_danh_gia { get; set; }
         public virtual san_pham? San_Pham { get; set; }
